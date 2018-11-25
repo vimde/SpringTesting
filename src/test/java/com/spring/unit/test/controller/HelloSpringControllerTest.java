bf9a1291978e5a55dd686708d0ca238892e76e5a
Vivek <vivekrmd@gmail.com>
2018-11-25 14:46:23 +0530

    Added another test with expected status and content - assertion is not required in this case

--- src/test/java/com/spring/unit/test/controller/HelloSpringControllerTest.java
+++ src/test/java/com/spring/unit/test/controller/HelloSpringControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(HelloSpringController.class)
@@ -32,5 +33,23 @@ public class HelloSpringControllerTest {
 		
 		// Assert that the result matches with the expected result
 		assertEquals("Hello Spring", result.getResponse().getContentAsString());
+	}
+	
+	@Test
+	public void shouldReturnHelloSpringWithExpectedContentAndStatusWhenHelloSpringEndpointIsAccessed() 
+			throws Exception {
+		
+		// Build the request with the endpoint and the content-type that is expected/accepted
+		RequestBuilder request = MockMvcRequestBuilders.get("/hello-spring")
+				.accept(MediaType.APPLICATION_JSON);
+		
+		// Call the endpoint with the request and added the expectations before returning the result
+		mockMvc.perform(request)
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().string("Hello Spring"))
+				.andReturn();
+		
+		// Assert is not required in this case since we have already added the expectations above
+		//assertEquals("Hello Spring", result.getResponse().getContentAsString());
 	}
 }
